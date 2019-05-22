c3ec2bcd7997cecccbef069873d44ad6c40d6eee
Kolton Starr <starrkolton@gmail.com>
2019-05-22 22:58:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -37,29 +37,23 @@ end
 
 def play(array)
 count = array.size
-
 puts "Please enter a song name or number:"
 input = gets.chomp
 count = array.size
-
-
-
 if input.to_i.between?(1,9) == false 
   puts "Invalid input, please try again"
 end
-
-
   array.each_with_index do |song, idx|
     if song == input
       puts "Playing #{song}" 
     elsif input.to_i == idx+1 
       puts "Playing #{song}" 
-
-
-
-    
     end 
   end
+end
+
+def exit_jukebox
+  puts "Goodbye"
 end
   
   
